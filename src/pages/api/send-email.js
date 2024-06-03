4ba7fd39ee1c67d2861b4194ed47bc13b5780cf8
Gus Meza <stavish@gmail.com>
2024-06-03 11:07:11 -0400

    new fix email

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     // Configuración del transporte
     const transporter = nodemailer.createTransport({
       host: 'smtp.titan.email', // Servidor SMTP de Titan Mail
-      port: 587, // Puerto SMTP (cambia a 465 si usas SSL/TLS) 587
+      port: 587, // Puerto SMTP (cambia a 465 si usas SSL/TLS)
       secure: false, // true para puerto 465, false para otros puertos
       auth: {
         user: process.env.EMAIL_USER, // Tu correo electrónico de Titan Mail
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
       res.status(200).json({ message: 'Correo enviado correctamente' });
     } catch (error) {
       console.error('Error enviando correo:', error);
-      res.status(500).json({ message: 'Error enviando correo' });
+      res.status(500).json({ message: 'Error enviando correo', error });
     }
   } else {
     res.status(405).json({ message: 'Método no permitido' });
