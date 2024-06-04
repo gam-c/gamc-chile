9525977cf27683dfef7377cc75e90c150d0e2acd
Gus Meza <stavish@gmail.com>
2024-06-04 10:37:14 -0400

    vercel speedinsights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,13 +2,15 @@ import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { Box } from '@mui/material';
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
+  
 function MyApp({ Component, pageProps }) {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Navbar />
       <Box component="main" sx={{ flex: 1 }}>
         <Component {...pageProps} />
+        <SpeedInsights />
       </Box>
       <Footer />
     </Box>
